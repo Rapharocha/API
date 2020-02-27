475d3cd7e98c7b5e28f39a50283f36bba069f261
Rapharocha <rapharocha2015@hotmail.com>
2020-02-27 14:59:41 -0300

    otimizando o codigo

---
+++ src/main/java/com/algaworks/socialbooks/service/exception/LivroNaoEncontradoException.java
@@ -0,0 +1,16 @@
+package com.algaworks.socialbooks.service.exception;
+
+public class LivroNaoEncontradoException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4529112685286480918L;
+	
+	public LivroNaoEncontradoException (String mensagem) {
+		super(mensagem);
+	}
+	public LivroNaoEncontradoException (String mensagem , Throwable causa) {
+		super(mensagem,causa);
+	}
+}
